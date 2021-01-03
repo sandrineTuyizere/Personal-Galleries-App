5a5822942835dd16794a086e63bfaed4693993ed
Sandrine Tuyizere <sandrauyizere@gmail.com>
2021-01-03 19:42:56 +0200

    adding image

--- galleries/admin.py
+++ galleries/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 # Register your models here.
 
 from .models import Photographer,Location,Image,Category
-# Register your models here.
+
 admin.site.register(Photographer)
 admin.site.register(Location)
 admin.site.register(Image)
